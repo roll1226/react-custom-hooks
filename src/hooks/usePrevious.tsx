29aafff12932b17d41dcd88987f607b17ca7c4d2
roll1226 <tomo_yasu_4724@icloud.com>
2024-08-11 08:20:47 +0900

    fix: UsePrevious page

--- src/hooks/usePrevious.tsx
+++ src/hooks/usePrevious.tsx
@@ -4,12 +4,16 @@ type UsePrevious = <T>(value: T) => T | undefined;
 
 export const usePrevious: UsePrevious = <T,>(value: T) => {
   const currentRef = useRef<T>(value);
+  const previousRef = useRef<T>(value);
 
   useEffect(() => {
+    console.log(value, currentRef.current, previousRef.current);
+
     if (currentRef.current !== value) {
+      previousRef.current = currentRef.current;
       currentRef.current = value;
     }
   }, [value]);
 
-  return currentRef.current;
+  return previousRef.current;
 };
