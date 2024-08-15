1bf4cccc40dd1402aecfdcdc8dc4e5aac87363cf
roll1226 <tomo_yasu_4724@icloud.com>
2024-08-16 06:55:13 +0900

    feat: comment to useEffectOnce

--- src/hooks/useEffectOnce.tsx
+++ src/hooks/useEffectOnce.tsx
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 type UseEffectOnce = (cb: React.EffectCallback) => void;
 
 export const useEffectOnce: UseEffectOnce = (cb) => {
+  /** TODO: StrictMode時はマウント処理が二回実行される */
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(cb, []);
 };
