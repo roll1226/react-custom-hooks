2cb45618c237d44cfee2afc2620a1c8fe0a1e9c4
roll1226 <tomo_yasu_4724@icloud.com>
2024-08-25 07:39:55 +0900

    change: text useStorage

--- src/pages/UseStorage.tsx
+++ src/pages/UseStorage.tsx
@@ -3,17 +3,17 @@ import { useLocalStorage, useSessionStorage } from "../hooks/useStorage";
 
 const UseStorage: FC = () => {
   const [name, setName, removeName] = useSessionStorage("name", "roll1226");
-  const [age, setAge, removeAge] = useLocalStorage("age", 26);
+  const [age, setAge, removeAge] = useLocalStorage("age", 25);
 
   return (
     <div>
       <div>
         {name} - {age}
       </div>
-      <button onClick={() => setName("John")}>Set Name</button>
-      <button onClick={() => setAge(40)}>Set Age</button>
-      <button onClick={removeName}>Remove Name</button>
-      <button onClick={removeAge}>Remove Age</button>
+      <button onClick={() => setName("ROLL")}>名前をセット</button>
+      <button onClick={() => setAge(30)}>年齢をセット</button>
+      <button onClick={removeName}>名前を削除</button>
+      <button onClick={removeAge}>年齢を削除</button>
     </div>
   );
 };
