e074c1b83b9f3f7af70351c831fc8a25f73cfc32
roll1226 <tomo_yasu_4724@icloud.com>
2024-08-31 20:35:17 +0900

    change: useToggle type

--- src/hooks/useToggle.tsx
+++ src/hooks/useToggle.tsx
@@ -1,19 +1,16 @@
 import { MouseEvent, useState } from "react";
 
-type UseToggleReturn = [
-  boolean,
-  (
-    value?: boolean | MouseEvent<HTMLButtonElement, globalThis.MouseEvent>
-  ) => void
-];
+type ToggleValue = (
+  value?: boolean | MouseEvent<HTMLButtonElement, globalThis.MouseEvent>
+) => void;
+
+type UseToggleReturn = [boolean, ToggleValue];
 
 type UseToggle = (defaultValue: boolean) => UseToggleReturn;
 
 export const useToggle: UseToggle = (defaultValue) => {
   const [value, setValue] = useState(defaultValue);
-  const toggleValue = (
-    value?: boolean | MouseEvent<HTMLButtonElement, globalThis.MouseEvent>
-  ) => {
+  const toggleValue: ToggleValue = (value) => {
     setValue((currentValue: boolean) =>
       typeof value === "boolean" ? value : !currentValue
     );
